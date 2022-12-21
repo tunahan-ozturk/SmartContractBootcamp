f2d4150f1e17ecf30e5dacd151d410eeffeb8fd0
Tunahan Öztürk <61948238+tnhn17@users.noreply.github.com>
2022-12-22 00:20:43 +0300

    Update counter.sol

--- .github/workflows/counter.sol
+++ .github/workflows/counter.sol
@@ -1,15 +1,15 @@
-// SPDX-License-Identifier:MIT
-pragma solidity ^0.8.7;
+// SPDX-License-Identifier:MIT // lisans tanımlama
+pragma solidity ^0.8.7; // versiyon
 
 contract Counter{
     uint public count;
 
-    function inc() external {
-        count += 1;
+    function inc() external { // sonradan kullanım için external kullandık
+        count += 1; //ekleme yapar
     }
 
-    function dec() external {
-        count -= 1;
+    function dec() external { // sonradan kullanım için external kullandık
+        count -= 1; // çıkartma yapar
     }
 
 }
